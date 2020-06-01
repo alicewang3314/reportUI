72fec9fe3bde006f1959d54dad82b8c5d9f28898
alicewang3314 <alice.wang3314@gmail.com>
2020-06-01 09:40:09 -0400

    update bug-detail-dialog.component.ts

--- src/app/components/bug-detail-dialog/bug-detail-dialog.component.ts
+++ src/app/components/bug-detail-dialog/bug-detail-dialog.component.ts
@@ -4,7 +4,7 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'bug-detail-dialog',
-  templateUrl: './bug-detail-dialog.html',
+  templateUrl: './bug-detail-dialog.component.html',
 })
 
 export class BugDetailsDialog {
